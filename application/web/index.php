eb831fee0cc00f9a3b44f92149d848bc9253f435
Daniil <danya.moiseev@inbox.ru>
2024-05-19 17:56:54 +1000

    composer install and db connect trying

--- application/web/index.php
+++ application/web/index.php
@@ -1,1 +1,7 @@
-<?php phpinfo();
+<?php
+
+$dsn = "host=localhost port=5432 dbname=docker_php user=docker-dan password=docker-dan options='--client_encoding=UTF8'";
+$dbConn = pg_connect($dsn);
+$result = pg_query($dbConn, "select * from pg_stat_activity");
+
+var_dump(pg_fetch_all($result));
